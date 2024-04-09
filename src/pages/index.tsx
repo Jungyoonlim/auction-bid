93b842d161cc6bb941faea6aa215a4536ba393c2
Joanne <jungyoon.lim99@gmail.com>
2024-04-09 11:34:55 -0700

    filter branch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,2 +1,7 @@
 'use client';
 import React, { useState } from 'react'; 
+import { BrowserRouter as Router, Route } from 'react-router';
+import { Container, Row, Col } from 'react-bootstrap';
+import AuctionDetail from '../components/AuctionDetail';
+import { GpuCluster } from '../components/GpuCluster';
+
